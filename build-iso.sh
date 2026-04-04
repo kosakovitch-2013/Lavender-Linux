6ab32152be45a43def5ace680b3f905afc21acdb
kosakovitch-2013 <kosavinna@gmail.com>
2026-04-04 21:17:15 +0300

    Add Lavender source files: init, kernel config, lavfetch, build script

---
+++ build-iso.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd /home/kot/LavenderOS/build
+cd rootfs && find . | cpio -o -H newc | gzip > ../initramfs.cpio.gz && cd ..
+cp initramfs.cpio.gz iso/boot/initramfs.gz
+grub-mkrescue -o lavender.iso iso/
+echo "ISO built: lavender.iso"
+
