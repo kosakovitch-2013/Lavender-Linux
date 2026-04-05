878a2bbc9c556490b89d17d0de2feb6a0032bbb9
kosakovitch-2013 <kosavinna@gmail.com>
2026-04-05 15:24:53 +0300

    Update build-iso.sh

--- build-iso.sh
+++ build-iso.sh
@@ -4,4 +4,4 @@ cd rootfs && find . | cpio -o -H newc | gzip > ../initramfs.cpio.gz && cd ..
 cp initramfs.cpio.gz iso/boot/initramfs.gz
 grub-mkrescue -o lavender.iso iso/
 echo "ISO built: lavender.iso"
-
+# so this script isn't automatic for the distro, the bash script is designed for you to open it so the iso would be automaticly created.
